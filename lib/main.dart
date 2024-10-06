245c6dc1974f5e564b4561f72199b691305d764a
KaiC5504 <kaichuan2004@gmail.com>
2024-10-06 16:05:20 +0800

    R&L no longer Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travelcustom/firebase_options.dart';
 import 'package:travelcustom/views/login_view.dart';
-import 'firebase_options.dart';
+import 'package:travelcustom/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,35 +21,13 @@ class MyApp extends StatelessWidget {
             seedColor: const Color.fromARGB(255, 136, 101, 197)),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     );
   }
 }
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
-
-  @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -65,111 +44,14 @@ class _RegisterViewState extends State<RegisterView> {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return Column(
-                children: [
-                  //username textfield
-                  Center(
-                    child: Container(
-                      width: 300,
-                      padding: const EdgeInsets.all(15),
-                      child: TextField(
-                        controller: _email,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                            labelText: 'Email', border: OutlineInputBorder()),
-                      ),
-                    ),
-                  ),
-
-                  //password textfield with show/hide password
-                  ShowPassword(
-                    PasswordController: _password,
-                  ),
-
-                  //register button
-                  ElevatedButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'weak-password') {
-                          print('Weak Password');
-                        } else if (e.code == 'email-already-in-use') {
-                          print('Email Registered');
-                        } else if (e.code == 'invalid-email') {
-                          print('Invalid Email');
-                        }
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 162, 136, 222),
-                      foregroundColor: Colors.white,
-                      elevation: 7,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 15),
-                    ),
-                    child: const Text('Register',
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                ],
-              );
+              print(FirebaseAuth.instance.currentUser);
+              print('Email: ${FirebaseAuth.instance.currentUser?.email}');
+              print('UID: ${FirebaseAuth.instance.currentUser?.uid}');
+              return const LoginView();
             default:
               return Text('App is Loading...');
           }
         },
-      ),
-    );
-  }
-}
-
-class ShowPassword extends StatefulWidget {
-  // ignore: non_constant_identifier_names
-  final TextEditingController PasswordController;
-
-  // ignore: non_constant_identifier_names
-  const ShowPassword({super.key, required this.PasswordController});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _ShowPasswordState createState() => _ShowPasswordState();
-}
-
-class _ShowPasswordState extends State<ShowPassword> {
-  bool _isObscured = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        padding: const EdgeInsets.all(15),
-        child: TextField(
-          controller: widget.PasswordController,
-          obscureText: _isObscured,
-          enableSuggestions: false,
-          autocorrect: false,
-          decoration: InputDecoration(
-            labelText: 'Password',
-            border: const OutlineInputBorder(),
-            suffixIcon: IconButton(
-                icon: Icon(
-                  _isObscured ? Icons.visibility_off : Icons.visibility,
-                  color: Colors.grey,
-                ),
-                onPressed: () {
-                  setState(() {
-                    _isObscured = !_isObscured;
-                  });
-                }),
-          ),
-        ),
       ),
     );
   }
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:travelcustom/firebase_options.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -30,75 +28,53 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:
-            const Text('TravelCustom', style: TextStyle(color: Colors.white)),
-        backgroundColor: const Color.fromARGB(255, 162, 136, 222),
-      ),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
+    return Column(
+      children: [
+        //username textfield
+        Center(
+          child: Container(
+            width: 300,
+            padding: const EdgeInsets.all(15),
+            child: TextField(
+              controller: _email,
+              enableSuggestions: false,
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  labelText: 'Email', border: OutlineInputBorder()),
+            ),
+          ),
         ),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  //username textfield
-                  Center(
-                    child: Container(
-                      width: 300,
-                      padding: const EdgeInsets.all(15),
-                      child: TextField(
-                        controller: _email,
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: const InputDecoration(
-                            labelText: 'Email', border: OutlineInputBorder()),
-                      ),
-                    ),
-                  ),
 
-                  //password textfield with show/hide password
-                  ShowPassword(
-                    PasswordController: _password,
-                  ),
+        //password textfield with show/hide password
+        ShowPassword(
+          PasswordController: _password,
+        ),
 
-                  //register button
-                  ElevatedButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                                email: email, password: password);
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == 'invalid-credential') {
-                          print('Wrong emai or password');
-                        }
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 162, 136, 222),
-                      foregroundColor: Colors.white,
-                      elevation: 7,
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 15),
-                    ),
-                    child: const Text('Login',
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                ],
-              );
-            default:
-              return Text('App is Loading...');
-          }
-        },
-      ),
+        //register button
+        ElevatedButton(
+          onPressed: () async {
+            final email = _email.text;
+            final password = _password.text;
+            try {
+              final userCredential = await FirebaseAuth.instance
+                  .signInWithEmailAndPassword(email: email, password: password);
+              print(userCredential);
+            } on FirebaseAuthException catch (e) {
+              if (e.code == 'invalid-credential') {
+                print('Wrong emai or password');
+              }
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 162, 136, 222),
+            foregroundColor: Colors.white,
+            elevation: 7,
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          ),
+          child: const Text('Login', style: TextStyle(color: Colors.white)),
+        ),
+      ],
     );
   }
 }
